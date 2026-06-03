86da1569d3f065c3b0cab71b1bde5f862854b7d5
aymlpz92 <aymericlopez.92@gmail.com>
2026-06-03 11:15:35 +0200

    feat: création du endpoint api/health (#1)

--- src/main/java/fr/maif/bookstore_api/BookstoreApiApplication.java
+++ src/main/java/fr/maif/bookstore_api/BookstoreApiApplication.java
@@ -2,8 +2,9 @@ package fr.maif.bookstore_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BookstoreApiApplication {
 
 	public static void main(String[] args) {
